c80786f6d2b9b1026285aa0f66bf55bc7e55f1ad
Roman Kvasnyj <kvasnyj@gmail.com>
2018-06-01 10:26:02 +0200

    Comment

--- configure_plugins.sh
+++ configure_plugins.sh
@@ -2,6 +2,6 @@
 wget --quiet http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
 sh ./call_jenkins_groovy.sh jenkins_git.groovy
 sh ./call_jenkins_groovy.sh setup_clean_clusters.groovy
-#sh ./call_jenkins_groovy.sh setup_ecs.groovy
+sh ./call_jenkins_groovy.sh setup_ecs.groovy
 #sh ./call_jenkins_groovy.sh setup_ec2.groovy
 echo 'DONE configuring plugins'
