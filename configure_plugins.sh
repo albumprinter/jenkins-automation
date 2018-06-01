7971fedabb816a84f47a3333cc23976d20744ee0
Roman Kvasnyj <kvasnyj@gmail.com>
2018-06-01 14:17:50 +0200

    Restore source code

--- configure_plugins.sh
+++ configure_plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-wget --quiet http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
+wget -N --quiet http://127.0.0.1:8080/jnlpJars/jenkins-cli.jar
 sh ./call_jenkins_groovy.sh jenkins_git.groovy
 sh ./call_jenkins_groovy.sh setup_clean_clusters.groovy
 sh ./call_jenkins_groovy.sh setup_ecs.groovy
